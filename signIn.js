916a36aacbfbc8dc0522668c091a05c6c142784e
Oguzhan <ozdemiroguzhan55@gmail.com>
2023-05-17 13:37:11 +0300

    login pages

---
+++ signIn.js
@@ -0,0 +1,61 @@
+var person = { name: "", surname: "", username: "", password: "", role: "" };
+var persons;
+var signInForm = document.getElementById("signInForm");
+var usernameCheck = false;
+
+signInForm.addEventListener("submit", signIn);
+
+if (localStorage.getItem('persons') === null) {
+    getJSONFile()
+}
+else {
+    persons = JSON.parse(localStorage.getItem('persons'));
+}
+
+
+
+function signIn(e) {
+    person.username = document.getElementById("username").value
+    person.password = document.getElementById("password").value
+    console.log("aa")
+    e.preventDefault(); // Sayfanın yeniden yüklenmesini önlemek için kullanılır
+    persons.forEach(data => {
+        if (person.username == data.username) {
+            usernameCheck = true;
+            if (person.password == data.password) {
+                person.name = data.name;
+                person.surname = data.surname;
+                person.role = data.role;
+                localStorage.setItem('user', JSON.stringify(person));
+                window.location.href = "main.html";
+            }
+            else {
+                alert("Invalid password!!");
+            }
+        }
+
+    });
+    if (!usernameCheck) {
+        usernameCheck = false;
+        alert("Username not found!! If you do not have an account, you can create a new account.")
+        return;
+    }
+    // Formdaki değerleri almak
+
+
+    // Diğer işlemleri burada gerçekleştirebilirsiniz
+    // Örneğin, bu verileri bir API'ye gönderme veya depolama işlemleri gibi
+
+}
+function getJSONFile() {
+    fetch("persons.json").then(function (response) {
+        return response.json()
+    }).then(function (datas) {
+        persons = datas;
+
+
+    }).catch(function (err) {
+        console.log(err);
+    })
+}
+
---
+++ signUp.js
@@ -0,0 +1,57 @@
+var person = { name: "", surname: "", username: "", password: "", role: "" };
+var persons;
+var usernameCheck = false;
+
+if (localStorage.getItem('persons') === null) {
+    getJSONFile()
+}
+else {
+    persons = JSON.parse(localStorage.getItem('persons'));
+}
+
+var signUpForm = document.getElementById("signUpForm");
+signUpForm.addEventListener("submit", signUp);
+
+function signUp(e) {
+    person.username = document.getElementById("username1").value;
+    person.name = document.getElementById("name").value;
+    person.surname = document.getElementById("surName").value;
+    person.password = document.getElementById("password1").value;
+    person.role = document.querySelector('input[name="role"]:checked').value;
+    e.preventDefault(); // Sayfanın yeniden yüklenmesini önlemek için kullanılır
+    persons.forEach(data => {
+        if (data.username == person.username) {
+            alert("This username is already in use!")
+            usernameCheck = true;
+        }
+
+    });
+    if (usernameCheck) {
+        usernameCheck = false;
+        return;
+    }
+    if (person.password != document.getElementById("passwordC").value) {
+        alert("Your passwords do not match, please try again!!")
+        return;
+    }
+
+    persons.push(person);
+    localStorage.setItem('persons', JSON.stringify(persons));
+    alert("Your registration has been successfully received. You are being redirected to the Sign in page...");
+    window.location.href = "signIn.html";
+
+    // Diğer işlemleri burada gerçekleştirebilirsiniz
+    // Örneğin, bu verileri bir API'ye gönderme veya depolama işlemleri gibi
+
+}
+function getJSONFile() {
+    fetch("persons.json").then(function (response) {
+        return response.json()
+    }).then(function (datas) {
+        persons = datas;
+
+
+    }).catch(function (err) {
+        console.log(err);
+    })
+}
