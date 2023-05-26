2a2a38e759dbe47757e2ca2848c37c33dffee9aa
İrem Çetiner <biiremcetiner@gmail.com>
2023-05-26 18:48:02 +0300

    validation

--- signUp.js
+++ signUp.js
@@ -1,7 +1,30 @@
 var person = { name: "", surname: "", username: "", password: "", role: "" };
 var persons;
 var usernameCheck = false;
+const passwordInput = document.querySelector("#password1");
+const requirementList = document.querySelectorAll(".requirement-list li");
 
+const requirements = [
+    {regex: /.{8,}/, index: 0},
+    {regex: /[0-9]/, index: 1},
+    {regex: /[a-z]/, index: 2},
+    {regex: /[^A-Za-z0-9]/, index: 3},
+    {regex: /[A-Z]/, index: 4},
+];
+passwordInput.addEventListener("keyup", (e) => {
+    requirements.forEach(item => {
+        const isValid = item.regex.test(e.target.value);
+        const requirementItem = requirementList[item.index];
+        if(isValid) {
+            requirementItem.firstElementChild.className="fa-solid fa-check";
+            requirementItem.classList.add("valid");
+        }else{
+          
+            requirementItem.firstElementChild.className = "fa-solid fa-circle";
+            requirementItem.classList.remove("valid");
+        }
+    })
+})
 if (localStorage.getItem('persons') === null) {
     getJSONFile()
 }
